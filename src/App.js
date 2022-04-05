1bba2f48dfdba51d9d9065573122617bed3c7926
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-04-05 22:44:55 +0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Header, Footer } from "./components";
+import * as pages from "./pages";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route index element={<pages.HomePage />} />
+        <Route path="login" element={<pages.AuthPage />} />
+        <Route path="register" element={<pages.AuthPage />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
