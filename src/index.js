cb0069261e7426ef78d8ae6b0c2ab1ed4f525ee6
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-04-09 19:58:47 +0800

    added react query client

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
