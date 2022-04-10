a6173d9c91f483b5e13c7d8ec332d67aba7d7a11
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-04-10 11:32:50 +0800

    article api

--- src/api/article.js
+++ src/api/article.js
@@ -6,3 +6,52 @@ export const listArticles = (params) => {
     .then(({ data }) => data)
     .catch((error) => JSON.parse(error));
 };
+
+export const feedArticles = () => {
+  return axiosInstance
+    .get("/articles/feed")
+    .then(({ data }) => data)
+    .catch((error) => JSON.parse(error));
+};
+
+export const getArticle = (slug) => {
+  return axiosInstance
+    .get(`/articles/${slug}`)
+    .then(({ data }) => data)
+    .catch((error) => JSON.parse(error));
+};
+
+export const createArticle = (data) => {
+  return axiosInstance
+    .post("/articles", data)
+    .then(({ data }) => data)
+    .catch((error) => JSON.parse(error));
+};
+
+export const updateArticle = (slug, data) => {
+  return axiosInstance
+    .put(`/articles/${slug}`, data)
+    .then(({ data }) => data)
+    .catch((error) => JSON.parse(error));
+};
+
+export const deleteArticle = (slug) => {
+  return axiosInstance
+    .delete(`/articles/${slug}`)
+    .then(({ data }) => data)
+    .catch((error) => JSON.parse(error));
+};
+
+export const favoriteArticle = (slug) => {
+  return axiosInstance
+    .post(`/articles/${slug}/favorite`)
+    .then(({ data }) => data)
+    .catch((error) => JSON.parse(error));
+};
+
+export const unfavoriteArticle = (slug) => {
+  return axiosInstance
+    .delete(`/articles/${slug}/favorite`)
+    .then(({ data }) => data)
+    .catch((error) => JSON.parse(error));
+};
