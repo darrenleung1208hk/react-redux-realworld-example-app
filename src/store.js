8040e272d407822590544ca518e31ec61af41811
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-04-18 13:10:12 +0800

    exported reducers and added to store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { articleSlice, articleListSlice } from "./reducers";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    article: articleSlice,
+    articleList: articleListSlice,
+  },
 });
