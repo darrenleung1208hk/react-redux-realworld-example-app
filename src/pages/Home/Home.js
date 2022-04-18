58e13da9f2f14147b134c3899fc7cf9ce200ef34
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-04-18 15:19:34 +0800

    render article previews dynamically

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,42 @@
 import React from "react";
 
+const articles = [
+  {
+    slug: "how-to-train-your-dragon",
+    title: "How to train your dragon",
+    description: "Ever wonder how?",
+    body: "It takes a Jacobian",
+    tagList: ["dragons", "training"],
+    createdAt: "2016-02-18T03:22:56.637Z",
+    updatedAt: "2016-02-18T03:48:35.824Z",
+    favorited: false,
+    favoritesCount: 0,
+    author: {
+      username: "jake",
+      bio: "I work at statefarm",
+      image: "https://i.stack.imgur.com/xHWG8.jpg",
+      following: false,
+    },
+  },
+  {
+    slug: "how-to-train-your-dragon-2",
+    title: "How to train your dragon 2",
+    description: "So toothless",
+    body: "It a dragon",
+    tagList: ["dragons", "training"],
+    createdAt: "2016-02-18T03:22:56.637Z",
+    updatedAt: "2016-02-18T03:48:35.824Z",
+    favorited: false,
+    favoritesCount: 0,
+    author: {
+      username: "jake",
+      bio: "I work at statefarm",
+      image: "https://i.stack.imgur.com/xHWG8.jpg",
+      following: false,
+    },
+  },
+];
+
 const popularTags = [
   "programning",
   "javascript",
@@ -31,6 +68,33 @@ const HomePage = () => {
     );
   };
 
+  const renderArticlePreview = () =>
+    articles.map((article, index) => {
+      return (
+        <div key={`article-preview-${index}`} className="article-preview">
+          <div className="article-meta">
+            <a href="profile.html">
+              <img src={article.author.image} />
+            </a>
+            <div className="info">
+              <a href="" className="author">
+                {article.author.username}
+              </a>
+              <span className="date">{article.createdAt}</span>
+            </div>
+            <button className="btn btn-outline-primary btn-sm pull-xs-right">
+              <i className="ion-heart"></i> {article.favoritesCount}
+            </button>
+          </div>
+          <a href="" className="preview-link">
+            <h1>{article.title}</h1>
+            <p>{article.description}</p>
+            <span>Read more...</span>
+          </a>
+        </div>
+      );
+    });
+
   return (
     <div className="home-page">
       <div className="banner">
@@ -58,52 +122,7 @@ const HomePage = () => {
               </ul>
             </div>
 
-            <div className="article-preview">
-              <div className="article-meta">
-                <a href="profile.html">
-                  <img src="http://i.imgur.com/Qr71crq.jpg" />
-                </a>
-                <div className="info">
-                  <a href="" className="author">
-                    Eric Simons
-                  </a>
-                  <span className="date">January 20th</span>
-                </div>
-                <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                  <i className="ion-heart"></i> 29
-                </button>
-              </div>
-              <a href="" className="preview-link">
-                <h1>How to build webapps that scale</h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-              </a>
-            </div>
-
-            <div className="article-preview">
-              <div className="article-meta">
-                <a href="profile.html">
-                  <img src="http://i.imgur.com/N4VcUeJ.jpg" />
-                </a>
-                <div className="info">
-                  <a href="" className="author">
-                    Albert Pai
-                  </a>
-                  <span className="date">January 20th</span>
-                </div>
-                <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                  <i className="ion-heart"></i> 32
-                </button>
-              </div>
-              <a href="" className="preview-link">
-                <h1>
-                  The song you won't ever stop singing. No matter how hard you
-                  try.
-                </h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-              </a>
-            </div>
+            {renderArticlePreview()}
           </div>
 
           <div className="col-md-3">{renderPopularTags()}</div>
